6bfacb83013748ab7108af9eec1b3495b7ead805
Aditya Rawat <rawataditya795@gmail.com>
2023-06-22 00:31:10 +0530

    changes in cartproducts.jsx

--- components/CartProducts.jsx
+++ components/CartProducts.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
 const CartProducts = ({ prod }) => {
+  console.log(prod);
   return (
-    <div>
-      {prod.products.map((pro) => (
-        <div key={pro.id}>
-          <h3>{pro.title}</h3>
-          <h3>{pro.price}</h3>
+    // Cards Grid
+    <div className="grid justify-items-start gap-x-3 px-2 sm:grid-cols-3 lg:grid-cols-5">
+        {prod.products.map((pro) => (
+        <div className="py-10" key={pro?.id}>
+        <div className=" overflow-hidden rounded shadow-lg">
+          <img src={pro?.images[0]} alt="mountain" className="object-cover w-full h-48" />
+          <div className="text-xl font-bold text-gray-700">{pro?.title}</div>
+            <div className="flex flex-col">
+              <p className="text-lg text-gray-700">${pro?.price}</p>
+              <button className="rounded bg-red-600 hover:bg-red-500 px-2 py-1 text-white">Buy Now</button>
+            </div>
+          </div>
         </div>
-      ))}
+        ))}
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import CartProducts from '@/components/CartProducts';
 export const getStaticProps = async () => {
   const res = await fetch('https://dummyjson.com/products?limit=10');
   const responseData = await res.json();
-
   return {
     props: {
       prod: responseData,
@@ -15,10 +14,12 @@ export const getStaticProps = async () => {
 const Home = ({ prod }) => {
   return (
     <div>
-      <h1 className="font-bold text-4xl">Search products</h1>
-      <div className="grid grid-cols-5">
-        <CartProducts prod={prod} />
+      <h1 class="mb-2 mt-10 text-center text-4xl">Search Product</h1>
+      <div class="justify-center flex items-center">
+        <input type="text" placeholder="Search..." class=" rounded-l-md border border-gray-300 px-4 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500" />
+        <button class="rounded-r-md bg-red-600 px-4 py-2 text-white">Search</button>
       </div>
+        <CartProducts prod={prod} />
     </div>
   );
 };
